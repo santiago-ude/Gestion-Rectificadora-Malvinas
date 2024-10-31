2851d21808a944e7485037a7990b9c0af81bbe8c
matias <matiasantonelli6874@gmail.com>
2024-10-31 14:17:03 -0300

    Clientes Service y NavBarComponent
    se termino de realizar la estructura del proyecto por ahora (NavbarComponent), y se realizo el service de clientes

--- TPFinalLaboIV/src/app/clientes/service/clientes.service.ts
+++ TPFinalLaboIV/src/app/clientes/service/clientes.service.ts
@@ -1,9 +1,69 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Clientes } from '../interface/clientes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  http = inject(HttpClient);
+  baseUrl = "http://localhost:3000/clientes";
+
+  obtenerClientes(): Observable<Clientes[]>{
+    return this.http.get<Clientes[]>(this.baseUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  obtenerClienteXDni(dni: string | undefined): Observable<Clientes>{
+    return this.http.get<Clientes>(`${this.baseUrl}/${dni}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  agregarCliente(cliente: Clientes): Observable<Clientes>{
+    return this.http.post<Clientes>(this.baseUrl, cliente).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  editarCliente(id: string | undefined, cliente: Clientes): Observable<Clientes>{
+    return this.http.put<Clientes>(`${this.baseUrl}/${id}`, cliente).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  borrarCliente(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+
+
+
+
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage: string;
+
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente
+      errorMessage = `Error del cliente: ${error.error.message}`;
+    } else {
+      // Error del lado del servidor
+      errorMessage = `${error.message}`;
+    }
+
+    // Mostrar error en consola
+    console.error('Ocurrió un error:', errorMessage);
+
+    // Retornar el mensaje de error como un Observable de error
+    return throwError(() => new Error(errorMessage));
+  }
+
+
 }
