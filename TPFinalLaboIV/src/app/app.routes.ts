58827ea82425ef5a1cb55123214b6b0d30b836a6
Santiago <santiude2004@gmail.com>
2024-11-08 12:54:32 -0300

    Pequeñas modificaciones
    Ruteado de listado de presupuestos al menu presupuestos.

--- TPFinalLaboIV/src/app/app.routes.ts
+++ TPFinalLaboIV/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { PresupuestosListComponent } from './presupuestos/components/presupuestos-list/presupuestos-list.component';
 
 export const routes: Routes = [
-    
+    {path: 'presupuestos', component: PresupuestosListComponent}
 ];
