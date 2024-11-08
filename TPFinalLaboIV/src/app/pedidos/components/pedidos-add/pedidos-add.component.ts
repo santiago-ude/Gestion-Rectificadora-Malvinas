eadcf3591dcba97cb6d67beaefb0044a28e259b2
Lautaro <lautaroarias02@gmail.com>
2024-11-08 12:31:05 -0300

    Rama del colo

--- TPFinalLaboIV/src/app/pedidos/components/pedidos-add/pedidos-add.component.ts
+++ TPFinalLaboIV/src/app/pedidos/components/pedidos-add/pedidos-add.component.ts
@@ -1,12 +1,83 @@
+<<<<<<< HEAD
 import { Component } from '@angular/core';
+=======
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Presupuesto } from '../../../presupuestos/interface/presupuesto';
+import { Pedidos } from '../../interface/pedidos';
+import { PedidoService } from '../../service/pedidos.service';
+import { PresupuestoService } from '../../../presupuestos/service/presupuesto.service';
+import { RouterModule } from '@angular/router';
+>>>>>>> parent of 64eef990 (cambios en pedidos)
 
 @Component({
   selector: 'app-pedidos-add',
   standalone: true,
+<<<<<<< HEAD
   imports: [],
+=======
+  imports: [ReactiveFormsModule, RouterModule],
+>>>>>>> parent of 64eef990 (cambios en pedidos)
   templateUrl: './pedidos-add.component.html',
   styleUrl: './pedidos-add.component.css'
 })
 export class PedidosAddComponent {
+<<<<<<< HEAD
 
 }
+=======
+  pedidoForm: FormGroup;
+  presupuestos: Presupuesto[] = [];
+  @Input() pedido?: Pedidos;
+  @Output() pedidoSaved = new EventEmitter<Pedidos>();
+
+  constructor(
+    private fb: FormBuilder,
+    private pedidoService: PedidoService,
+    private presupuestoService: PresupuestoService
+  ) {
+    this.pedidoForm = this.fb.group({
+      clienteId: ['', Validators.required],
+      fechaEntrada: ['', Validators.required],
+      fechaSalidaEstimada: ['', Validators.required],
+      estado: ['activo', Validators.required],
+      marcaAuto: ['', Validators.required],
+      modeloAuto: ['', Validators.required],
+      numeroSerie: ['', Validators.required],
+      descripcion: [''],
+      presupuesto: [null]
+    });
+  }
+
+  ngOnInit() {
+    this.cargarPresupuestos();
+    if (this.pedido) {
+      this.pedidoForm.patchValue(this.pedido);
+    }
+  }
+
+  cargarPresupuestos() {
+    this.presupuestoService.getPresupuestos().subscribe({
+      next: (presupuestos) => (this.presupuestos = presupuestos),
+      error: (error) => console.error('Error al cargar presupuestos:', error)
+    });
+  }
+
+  onSubmit() {
+    if (this.pedidoForm.valid) {
+      const pedidoData: Pedidos = this.pedidoForm.value;
+      if (this.pedido) {
+        this.pedidoService.updatePedido(this.pedido.id, pedidoData).subscribe({
+          next: (pedido) => this.pedidoSaved.emit(pedido),
+          error: (error) => console.error('Error al actualizar pedido:', error)
+        });
+      } else {
+        this.pedidoService.addPedido(pedidoData).subscribe({
+          next: (pedido) => this.pedidoSaved.emit(pedido),
+          error: (error) => console.error('Error al agregar pedido:', error)
+        });
+      }
+    }
+  }
+}
+>>>>>>> parent of 64eef990 (cambios en pedidos)
--- TPFinalLaboIV/src/app/pedidos/interface/pedidos.ts
+++ TPFinalLaboIV/src/app/pedidos/interface/pedidos.ts
@@ -1,2 +1,17 @@
 export interface Pedidos {
+<<<<<<< HEAD
 }
+=======
+  id: string;
+  cliente: Clientes;  
+  fechaEntrada: Date;
+  fechaSalidaEstimada: Date;
+  fechaEntregaEfectiva?: Date;
+  estado: 'activo' | 'entregado' | 'atrasado';
+  presupuesto?: Presupuesto;
+  marcaAuto: string;
+  modeloAuto: string;
+  numeroSerie: string;
+  descripcion: string;
+}
+>>>>>>> parent of 64eef990 (cambios en pedidos)
