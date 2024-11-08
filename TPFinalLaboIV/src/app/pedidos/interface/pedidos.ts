64eef990793150f09cf57eb744ef411cca9baeee
matias <matiasantonelli6874@gmail.com>
2024-11-08 11:59:58 -0300

    cambios en pedidos

--- TPFinalLaboIV/src/app/pedidos/interface/pedidos.ts
+++ TPFinalLaboIV/src/app/pedidos/interface/pedidos.ts
@@ -2,13 +2,13 @@ import { Clientes } from '../../clientes/interface/clientes';
 import { Presupuesto } from '../../presupuestos/interface/presupuesto';
 
 export interface Pedidos {
-  id: string;
-  cliente: Clientes;  
+  id?: string;
+  cliente: Clientes;
   fechaEntrada: Date;
   fechaSalidaEstimada: Date;
   fechaEntregaEfectiva?: Date;
   estado: 'activo' | 'entregado' | 'atrasado';
-  presupuesto?: Presupuesto;
+  presupuesto: Presupuesto;  
   marcaAuto: string;
   modeloAuto: string;
   numeroSerie: string;
