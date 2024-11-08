7c30dee6dd384633647ed376ec5b711ef457ae35
Lautaro <lautaroarias02@gmail.com>
2024-11-08 11:20:01 -0300

    changes

--- TPFinalLaboIV/src/app/app.component.ts
+++ TPFinalLaboIV/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ClientesPageComponent } from "./clientes/pages/clientes-page/clientes-p
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PageComponent, ClientesPageComponent],
+  imports: [RouterOutlet, ClientesPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- TPFinalLaboIV/src/app/clientes/components/cliente-list/cliente-list.component.ts
+++ TPFinalLaboIV/src/app/clientes/components/cliente-list/cliente-list.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './cliente-list.component.css'
 })
 export class ClienteListComponent {
+  
 
 }
