692a9ca7bc68cc5f8604f423f8390a72fd7d643b
matias <matiasantonelli6874@gmail.com>
2024-11-10 20:17:31 -0300

    cambios en el footer

--- TPFinalLaboIV/src/app/landingPage/footer/footer.component.ts
+++ TPFinalLaboIV/src/app/landingPage/footer/footer.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  dentroP1:boolean = false;
+  onMouseOverP1(){
+    this.dentroP1 = true;
+  }
+  onMouseLeaverP1(){
+    this.dentroP1 = false;
+  }
 }
