69fbabc4065991886e9f747e058986b2895d1e32
Santiago <santiude2004@gmail.com>
2025-01-29 22:40:43 -0300

    Update item-add.component.ts

--- TPFinalLaboIV/src/app/presupuestos/components/item-add/item-add.component.ts
+++ TPFinalLaboIV/src/app/presupuestos/components/item-add/item-add.component.ts
@@ -14,30 +14,44 @@ export class ItemAddComponent {
 
 
   @Output()
-  emitirEvento : EventEmitter<Item> = new EventEmitter();
+  emitirEvento: EventEmitter<Item> = new EventEmitter();
 
   FB = inject(FormBuilder);
 
-  
+
   formulario = this.FB.nonNullable.group({
     nombre: ['', [Validators.required, Validators.minLength(2)]],
     descripcion: ['', [Validators.required, Validators.minLength(2)]],
-    precioUnitario: [0, [Validators.required, Validators.min(0)]],  
-    precioManoObra: [0, [Validators.required, Validators.min(0)]],  
-    precioFinal: [0, [Validators.required, Validators.min(0)]], 
+    precioUnitario: [0, [Validators.required, Validators.min(0)]],
+    precioManoObra: [0, [Validators.required, Validators.min(0)]],
+    precioFinal: [0, [Validators.required, Validators.min(0)]],
   });
 
 
   //Emite el evento con el item cargado en el formulario
-  addItem(){
+  addItem() {
 
-    if(this.formulario.invalid)return;
+    if (this.formulario.invalid) return;
+
+    // Normalizar los valores de los campos string
+    Object.keys(this.formulario.controls).forEach((campo) => {
+      const control = this.formulario.get(campo);
+      if (control && typeof control.value === 'string') {
+        control.setValue(this.capitalizeFirstLetter(control.value), { emitEvent: false });
+      }
+    });
 
     const item = this.formulario.getRawValue();
 
     this.emitirEvento.emit(item);
     this.formulario.reset();
 
+  }
+
+
+  // Función para capitalizar la primera letra de cada campo string
+  capitalizeFirstLetter(value: string): string {
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
   }
 
 
