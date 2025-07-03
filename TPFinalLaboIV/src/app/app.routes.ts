a982145dbc0481ce06fbb3efb832df42e2669c67
matias <matiasantonelli6874@gmail.com>
2025-07-03 13:09:53 -0300

    cambios
    agregados front +responsive, problemas con la id en formularios

--- TPFinalLaboIV/src/app/app.routes.ts
+++ TPFinalLaboIV/src/app/app.routes.ts
@@ -1,36 +1,39 @@
-import { UpdatePedidosPageComponent } from './pedidos/pages/update-pedidos-page/update-pedidos-page.component';
-import { ClienteListComponent } from './clientes/components/cliente-list/cliente-list.component';
 import { Routes } from '@angular/router';
-import { ClienteAddComponent } from './clientes/components/cliente-add/cliente-add.component';
-import { CuerpoComponent } from './landingPage/cuerpo/cuerpo.component';
-import { PedidosPageComponent } from './pedidos/pages/pedidos-page/pedidos-page.component';
-import { PresupuestoPageComponent } from './presupuestos/pages/presupuesto-page/presupuesto-page.component';
-import { ListPedidosPageComponent } from './pedidos/pages/list-pedidos-page/list-pedidos-page.component';
-import { UpdatePageComponent } from './clientes/pages/update-page/update-page.component';
-import {NavbarGuard} from "./navbar.guard";
+import { NavbarGuard } from './navbar.guard';
+
+// Componentes
 import { PageComponent } from './landingPage/page/page.component';
+import { CuerpoComponent } from './landingPage/cuerpo/cuerpo.component';
+import { ClienteListComponent } from './clientes/components/cliente-list/cliente-list.component';
+import { ClienteAddComponent } from './clientes/components/cliente-add/cliente-add.component';
+import { UpdatePageComponent } from './clientes/pages/update-page/update-page.component';
+import { PedidosPageComponent } from './pedidos/pages/pedidos-page/pedidos-page.component';
+import { ListPedidosPageComponent } from './pedidos/pages/list-pedidos-page/list-pedidos-page.component';
+import { UpdatePedidosPageComponent } from './pedidos/pages/update-pedidos-page/update-pedidos-page.component';
+import { PresupuestoPageComponent } from './presupuestos/pages/presupuesto-page/presupuesto-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: PageComponent,
     children: [
+      // Ruta principal (landing)
       { path: '', component: CuerpoComponent },
 
       // Clientes
-      { path: 'clientes', component: ClienteListComponent },
-      { path: 'crearCliente', component: ClienteAddComponent },
-      { path: 'clientes/update/:id', component: UpdatePageComponent },
+      { path: 'clientes', component: ClienteListComponent, canActivate: [NavbarGuard] },
+      { path: 'crearCliente', component: ClienteAddComponent, canActivate: [NavbarGuard] },
+      { path: 'clientes/update/:id', component: UpdatePageComponent, canActivate: [NavbarGuard] },
 
       // Pedidos
-      { path: 'pedidos', component: ListPedidosPageComponent },
-      { path: 'addPedido', component: PedidosPageComponent },
-      { path: 'pedidos/update/:id', component: UpdatePedidosPageComponent },
+      { path: 'pedidos', component: ListPedidosPageComponent, canActivate: [NavbarGuard] },
+      { path: 'addPedido', component: PedidosPageComponent, canActivate: [NavbarGuard] },
+      { path: 'pedidos/update/:id', component: UpdatePedidosPageComponent, canActivate: [NavbarGuard] },
 
       // Presupuestos
-      { path: 'presupuestos', component: PresupuestoPageComponent },
+      { path: 'presupuestos', component: PresupuestoPageComponent, canActivate: [NavbarGuard] },
 
-      // Inicio
+      // Catch-all
       { path: '**', redirectTo: '' }
     ]
   }
