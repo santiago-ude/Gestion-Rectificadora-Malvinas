4ee1cb47f6063edae92a49751a0d4ae0d9f8433f
Santiago <santiude2004@gmail.com>
2025-07-09 18:23:20 -0300

    Update update-pedidos-page.component.ts

--- TPFinalLaboIV/src/app/pedidos/pages/update-pedidos-page/update-pedidos-page.component.ts
+++ TPFinalLaboIV/src/app/pedidos/pages/update-pedidos-page/update-pedidos-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PedidosUpdateComponent } from '../../components/pedidos-update/pedidos-update.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-pedidos-page',
   standalone: true,
-  imports: [PedidosUpdateComponent],
+  imports: [PedidosUpdateComponent, CommonModule],
   templateUrl: './update-pedidos-page.component.html',
   styleUrl: './update-pedidos-page.component.css'
 })
